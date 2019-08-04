4746bdb3b2f0092666a53af40cc0fcf53551a257
Hamza Haroon <hamzaharoon1314@gmail.com>
2019-08-04 21:18:20 +0500

    SUPERFB FOR HACKING FACEBOOK WITH Bruteforce
    #SUPERFB
    PROJECT TO HACK FB ACCOUNTS OF YOUR FRIEND LIST OR MEMBERS OF ANY GROUPS BY  Bruteforce

---
+++ SUPERFB.sh
@@ -0,0 +1,59 @@
+###SCRIPT SuperFB By DarkNoobHunter
+#!/bin/bash
+#version Final
+#jUST TYPE Y IF THEY ASK FOR INSTALLITION
+#This code for Kali Linux 
+apt update && apt upgrade 
+apt install git
+apt install php
+apt install wget
+apt install figlet
+apt install toilet
+apt install python2
+apt install python
+apt install ruby
+gem install lolcat
+pip2 install requests
+pip2 install termcolor
+apt install apache2
+apt install unzip
+apt install python2-dev git
+apt install git
+pip2 install mechanize
+pip2 install requests
+
+#This code for Termux 
+pkg update
+pkg upgrade
+pkg install git
+pkg install php
+pkg install wget
+pkg install figlet
+pkg install toilet
+pkg install python2
+pkg install python
+pkg install ruby
+gem install lolcat
+pip2 install requests
+pip2 install termcolor
+apt install openssh
+apt install curl
+apt install git
+pip2 install mechanize
+pip2 install requests
+#Done 
+sleep 1
+
+echo -e $g"▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒    "   
+echo -e $g"   ▒▒▒    ▒▒▒   ▒▒▒   ▒▒▒    ▒▒▒     ▒▒▒   "
+echo -e $g"▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒           " 
+echo -e $bl"."
+echo -e $bu "________________________________________"
+echo -e $bu  " Tools       : SUPERFB          " 
+echo -e $bu  " Author      : DarkNoobHunter  " 
+echo -e $bu  " Contact     : GITHUB " 
+echo -e $bu  "Thank,s for use this tools :)  "
+
+echo -e $bu  "_______________________________________" 
+sleep 1
+
